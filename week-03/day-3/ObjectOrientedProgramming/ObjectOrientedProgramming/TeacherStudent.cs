30a29dc7d657bbb9ed5ff7fdf971375bedd3bdce
C33thr33 <l.malek@outlook.cz>
2019-09-27 01:17:55 +0200

    Added OOP exercises - week 03 - day 3

---
+++ week-03/day-3/ObjectOrientedProgramming/ObjectOrientedProgramming/TeacherStudent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Teacher firstTeacher = new Teacher();
+            //Student studentOne = new Student("Pepa");
+            Student studentTwo = new Student();
+
+            studentTwo.Learn();
+            studentTwo.Question(firstTeacher);
+            firstTeacher.Teach(studentTwo);
+            firstTeacher.Answer();
+            Console.ReadKey();
+        }
+    }
+}
