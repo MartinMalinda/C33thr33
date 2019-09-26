9d51205ffc78fa14e0897fadc78007f45c4e4c79
C33thr33 <l.malek@outlook.cz>
2019-09-27 01:17:55 +0200

    Added OOP exercises - week 03 - day 4

---
+++ week-03/day-3/ObjectOrientedProgramming/ObjectOrientedProgramming/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedProgramming
+{
+    public class Student
+    {
+        public Student()
+        {
+
+        }
+        public Student(string name)
+        {
+
+        }
+
+        public void Learn()
+        {
+            Console.WriteLine("Student used Learn! It is not very effective... ");
+        }
+        public void Question(Teacher teacher)
+        {
+            teacher.Answer();
+        }
+
+    }
+}
---
+++ week-03/day-3/ObjectOrientedProgramming/ObjectOrientedProgramming/Teacher.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedProgramming
+{
+    public class Teacher
+    {
+        public void Teach(Student student)
+        {
+            student.Learn();
+        }
+        public void Answer()
+        {
+            Console.WriteLine("Teacher answered the quesiton");
+        }
+    }
+}
