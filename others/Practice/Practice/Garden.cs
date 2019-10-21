69adf2b4ecf58ca7a0ee01a5ceeb508e51f98fe3
C33thr33 <l.malek@outlook.cz>
2019-10-21 11:28:02 +0200

    Update - Garden - W4 - D2

---
+++ others/Practice/Practice/Garden.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    class Garden
+    {
+        Flower flower1 = new Flower("yellow");
+        Flower flower2 = new Flower("blue");
+        Tree tree1 = new Tree("purple");
+        Tree tree2 = new Tree("orange");
+
+
+        public List<Plant> plants = new List<Plant>();
+        
+
+        
+
+
+        
+    }
+
+    public abstract class Plant
+    {
+        protected int CurrentWaterLevel { get; set; } = 0;
+        protected int neededWaterLevel;
+        protected string Color { get; set; }
+
+        public Plant(string color)
+        {
+            Color = color;
+        }
+    }
+    public class Flower : Plant
+    {
+        public Flower(string color):base (color)
+        {
+            neededWaterLevel = 5;
+        }
+    }
+    public class Tree : Plant
+    {
+        public Tree(string color) : base(color)
+        {
+            neededWaterLevel = 10;
+        }
+    }
+}
---
+++ others/Practice/Practice/StringManipulation.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    class StringManipulation
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first word: ");
+            string wordOne = Console.ReadLine();
+            Console.Write("Enter second word: ");
+            string wordTwo = Console.ReadLine();
+            Anagram(wordOne, wordTwo);
+            Palindrome(wordOne, wordTwo);
+
+            Console.ReadKey();
+        }
+        public static void Palindrome(string first, string second)
+        {
+            first = first.ToLower();
+            second = second.ToLower();
+            bool Pal = true;
+            for (int i = 0; i < first.Length; i++)
+            {
+                if (first[i] != second[second.Length-i-1])
+                {
+                    Pal = false;
+                }
+            }
+            if (Pal)
+            {
+                Console.WriteLine($"{first} and {second} are palindromes");
+            }
+            else
+            {
+                Console.WriteLine($"{first} and {second} are NOT palindromes");
+            }
+        }
+        public static void Anagram(string first, string second)
+        {
+            char[] charOne = first.ToLower().ToCharArray();
+            char[] charTwo = second.ToLower().ToCharArray();
+            Array.Sort(charOne);
+            Array.Sort(charTwo);
+
+            if (charOne.ToString() == charTwo.ToString())
+            {
+                Console.WriteLine($"{first} and {second} are anagrams");
+            }
+            else
+            {
+                Console.WriteLine($"{first} and {second} are NOT anagrams");
+            }
+        }
+    }
+}
