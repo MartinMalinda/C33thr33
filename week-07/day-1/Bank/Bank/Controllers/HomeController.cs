96a25370fcb868e51ee68b2609e000951f3c2702
C33thr33 <l.malek@outlook.cz>
2019-10-29 16:44:08 +0100

    Add - Bank of Simba - W7 - D2

---
+++ week-07/day-1/Bank/Bank/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bank.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bank.Controllers
+{
+    public class HomeController : Controller
+    {
+        static List<BankAccount> listOfPeople = new List<BankAccount>() {
+        new BankAccount("Petr Kellner", 270000000000, "businessman", false),
+        new BankAccount("Andrej Babiš", 71000000000, "criminal", true),
+        new BankAccount("Donald Trump", 64495000000, "useful idiot", true),
+        new BankAccount("Elon Musk", 486000000000, "visionary", false),
+        new BankAccount("Jaromír Jágr", 1611700000, "hockey player", false)
+        };
+
+        [Route("")]
+        public IActionResult Home()
+        {
+            return View();
+        }
+
+        [Route("Index")]
+        public IActionResult Index()
+        {
+            return View(listOfPeople);
+        }
+
+        [HttpPost("CreateNew")]
+        public IActionResult Create(string name, double balance, string occupation, bool badGuy)
+        {
+            listOfPeople.Add(new BankAccount(name,balance,occupation,badGuy));
+            return RedirectToAction("Index");
+        }
+
+        [Route("Test")]
+        public IActionResult Test()
+        {
+            return View();
+        }
+    }
+}
---
+++ week-07/day-1/Bank/Bank/Models/BankAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bank.Models
+{
+    public class BankAccount
+    {
+
+        public string Name { get; }
+        public double Balance { get; }
+        public string Occupation { get; }
+        public int Id { get; }
+        public bool BadGuy { get; }
+
+        public BankAccount(string name, double balance, string occupation, bool badGuy)
+        {
+            Name = name;
+            Balance = balance;
+            Occupation = occupation;
+            BadGuy = badGuy;
+        }
+
+        public BankAccount()
+        {
+
+        }
+    }
+}
