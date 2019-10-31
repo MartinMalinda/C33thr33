9c00875fb4115916049d035fab9c7e741787b24b
C33thr33 <l.malek@outlook.cz>
2019-10-31 17:14:28 +0100

    Add - Dependency Injection - W7 - D4

---
+++ week-07/day-4/DI/DI/Models/UtilityService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DI.Models
+{
+    public class UtilityService
+    {
+        private readonly List<string> colors;
+        private readonly Random random;
+
+        public UtilityService()
+        {
+            colors = new List<string>
+        {
+            "red",
+            "blue",
+            "lime",
+            "orange",
+            "magenta"
+        };
+
+            random = new Random();
+        }
+
+        public string RandomColor()
+        {
+            return colors[random.Next(colors.Count)];
+        }
+        public List<string> ValidateEmail(string email)
+        {
+            string isValid;
+            string color;
+            List<string> combo = new List<string>();
+            if (email.Contains("@") && email.Contains("."))
+            {
+                isValid = email + " is a valid e-mail.";
+                color = "green";
+            }
+            else
+            {
+                isValid = email + " is not a valid e-mail.";
+                color = "red";
+            }
+            combo.Add(isValid);
+            combo.Add(color);
+            return combo;
+        }
+    }
+}
---
+++ week-07/day-4/DI/DI/Startup.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DI.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DI
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddTransient<ConsoleLoggerMiddleware>();
+            services.AddSingleton<Printer>();
+            services.AddSingleton<IColor,BlueColor>();
+            services.AddSingleton<UtilityService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseMvc();
+                app.UseMiddleware<ConsoleLoggerMiddleware>();
+            }
+            app.UseStaticFiles();
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
