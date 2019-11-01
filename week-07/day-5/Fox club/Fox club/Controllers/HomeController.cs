3f299c4cd32586a4d612f3344378766812171083
C33thr33 <l.malek@outlook.cz>
2019-11-01 17:24:56 +0100

    Add - Fox club - W7 - D5

---
+++ week-07/day-5/Fox club/Fox club/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Foxclub.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fox_club.Controllers
+{
+    public class HomeController : Controller
+    {
+        private LoFService LoFS { get; set; }
+        public HomeController(LoFService LoFS)
+        {
+            this.LoFS = LoFS;
+        }
+
+        [HttpGet("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login(string name)
+        {
+            LoFS.AddFox(name);
+            return RedirectToAction("information", "Home", new { Name = name });
+        }
+
+        [HttpGet("")]
+        public IActionResult Information(string name)
+        {
+            if (name == null)
+            {
+                return RedirectToAction("login");
+            }
+            return View(LoFS.FindFoxByName(name));
+        }
+
+        [HttpGet("nutrition")]
+        public IActionResult Nutrition(string name)
+        {
+            if (name == null)
+            {
+                return RedirectToAction("login");
+            }
+            return View(LoFS.FindFoxByName(name));
+        }
+
+        [HttpGet("nutrition")]
+        public IActionResult NutritionSet(string name)
+        {
+            if (name == null)
+            {
+                return RedirectToAction("login");
+            }
+            return View(LoFS.FindFoxByName(name));
+        }
+
+
+
+
+        //[Route("allfoxes")]
+        //public IActionResult Allfoxes()
+        //{
+        //    return View();
+        //}
+    }
+}
---
+++ week-07/day-5/Fox club/Fox club/Models/Fox.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Foxclub.Models
+{
+    public class Fox
+    {
+        public enum Tricks { KungFu, HTML, BackFlip };
+
+        public string name { get; private set; }
+        public string food { get; private set; } 
+        public string drink { get; private set; } 
+        public List<Tricks> ListOfTricks { get; private set; }
+
+        public Fox(string name)
+        {
+            this.name = name;
+            food = "pizza";
+            drink = "red bull";
+            ListOfTricks = new List<Tricks>();
+            ListOfTricks.Add(Tricks.BackFlip);
+        }
+
+        public void ChangeFood (string food)
+        {
+            this.food = food;
+        }
+
+        public void ChangeDrink (string drink)
+        {
+            this.drink = drink;
+        }
+    }
+}
